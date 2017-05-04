bce6423ab34183cb1b0b980f6e1d516437039318
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-04 10:01:47 -0300

    puse las variables, funciones, etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-main.js
+
+function Pokemon(nombre, color, nivelDeAmistad, vida,poderDeAtaque){
+	this.nombre =nombre;
+	this.color=color;
+	this.nivelDeAmistad = nivelDeAmistad;
+	this.vida = vida;
+	this.poderDeAtaque = poderDeAtaque;
+	this.mostrarPokemon = function(){
+		return ("Hola, soy: "+this.nombre+ " y soy de color "+ this.color)
+	}
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad =this.nivelDeAmistad + valor
+	}
+	this.atacar = function(pokemon){
+		pokemon.vida =pokemon.vida -this.poderDeAtaque
+	}
+}
+
+var pikachu=new Pokemon("Pikachu", "amarillo",100)
+var charmander=new Pokemon("Charmander", "rojo",20)
+
+pikachu.atacar
+console.log(charmander.vida)

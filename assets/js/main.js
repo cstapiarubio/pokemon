d3bb0d62e5a4a7c39dca2e8d974a5422fa117e7f
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-04 15:08:44 -0300

    trabajo final que imprime el resultado en pantalla

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-function Pokemon(nombre, color, nivelDeAmistad, vida,poderDeAtaque){
+function Pokemon(nombre, color, nivelDeAmistad, vida, poderDeAtaque){
 	this.nombre =nombre;
 	this.color=color;
 	this.nivelDeAmistad = nivelDeAmistad;
@@ -16,14 +16,30 @@ function Pokemon(nombre, color, nivelDeAmistad, vida,poderDeAtaque){
 	}
 }
 
-var pikachu=new Pokemon("Pikachu", "amarillo",0,100,10)
-var charmander=new Pokemon("Charmander", "rojo",0,20,80)
-var squartle=new Pokemon("Squartle", "azul",0,80,50)
-var charmander=new Pokemon("Bulbasaur", "verde",0,70,180)
+/*var pikachu=new Pokemon("Pikachu", "amarillo",0,100,10);
+var charmander=new Pokemon("Charmander", "rojo",0,20,80);
+var squartle=new Pokemon("Squartle", "azul",0,80,50);
+var charmander=new Pokemon("Bulbasaur", "verde",0,70,180);*/
 
-function pelear(pokemon1, pokemon2){
-	
+
+function pelear(){
+	var poke1 = document.getElementById("pokemon1").value;
+	var poke2 = document.getElementById("pokemon2").value;
+
+	var peleador1 =new Pokemon(poke1, "color pokemon",0,100,10);
+	var peleador2 =new Pokemon(poke2,"color pokemon",0,20,80);
+
+	if (peleador1.nombre==peleador2.nombre){
+		alert("Error, no puedes elegir el mismo pokemon")
+	}else{ 
+
+	peleador1.atacar(peleador2);
+    var espacioPelea= document.getElementById("espacio");
+	var mensaje = (peleador1.nombre + " atacó a " + peleador2.nombre + " y le queda " + peleador2.vida + " de vida restante");
+	espacioPelea.innerHTML =mensaje;
+	}
+//alert (peleador1.nombre + " atacó a " + peleador2.nombre + " y a " + peleador2.nombre + " le queda "+ peleador2.vida + " de vida restante")
 }
 
-pikachu.atacar(charmander)
-console.log(charmander.vida)
+//pikachu.atacar(charmander)
+//console.log(charmander.vida)
